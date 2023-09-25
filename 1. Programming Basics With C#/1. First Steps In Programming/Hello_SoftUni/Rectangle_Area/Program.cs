0d3665ff13f004dc05bf0bf810e260b4e9f4e86a
mitevaAlex <mitevaalexandra04@gmail.com>
2023-09-25 20:57:21 +0300

    Add files

---
+++ 1. Programming Basics With C#/1. First Steps In Programming/Hello_SoftUni/Rectangle_Area/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rectangle_Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            Console.WriteLine(a * b);
+        }
+    }
+}
---
+++ 1. Programming Basics With C#/1. First Steps In Programming/purvi-urok/purvi-urok-3zad/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace purvi_urok_3zad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(a * b);
+
+        }
+    }
+}
